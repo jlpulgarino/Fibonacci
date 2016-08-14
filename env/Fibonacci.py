acff6baa727fa2bf324ff9de3b7eb3fbb4490333
Jose Luis Pulgarin <jl.pulgarin@uniandes.edu.co>
2016-08-14 15:47:13 -0500

    Retorno inicial

--- env/Fibonacci.py
+++ env/Fibonacci.py
@@ -1,7 +1,6 @@
 # Entrada numero entero
 numero = int(raw_input("Escriba su numero por favor "))
 if numero > 1:
-    secuencia = set()
     numAnerior = 0
     numActual = 1
     #Itera hasta que se alcance el valor digitado
@@ -9,5 +8,4 @@ if numero > 1:
         suma = numActual + numAnerior
         numAnerior = numActual
         numActual = suma
-        secuencia.add(numActual)
-    print secuencia
+        print numActual
